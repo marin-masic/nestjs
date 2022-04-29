bb18c47fd7c6bdea857c3f0d1590e67789aa3936
Marin Masic <marin.masic@endava.com>
2022-04-29 12:42:36 +0200

    Section 5: Auth Part 1 - Authentication

---
+++ task-managent/src/auth/users.repository.ts
@@ -0,0 +1,25 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { EntityRepository, Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { User } from "./user.entity";
+import * as bcrypt from 'bcrypt';
+
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hash });
+
+    try {
+      await this.save(user);      
+    } catch (error) {
+      console.log(error.code)
+      if (error.code === '23505') throw new ConflictException('Username already exists');
+      throw new InternalServerErrorException();
+    }
+  }
+}
