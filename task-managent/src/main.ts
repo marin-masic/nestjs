f5b996eead42ebb0bb8d6dd618cbd0cfd631a939
Marin Masic <mamasic@gmail.com>
2022-05-06 10:46:23 +0200

    - add generic Express route and middleware

--- task-managent/src/main.ts
+++ task-managent/src/main.ts
@@ -7,6 +7,10 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  
+  //Add generic Express route with middleware ;)
+  app.getHttpAdapter().get('/test', (req, res) => res.send('bravo'));
+  
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
